7e956f9c0936beeb1f91e544aebec30bd6e6bc6f
K <K@192.168.109.106>
2016-05-21 20:35:25 +0100

    Florian 11/5/2016 CIT 260

--- DarkDungeon/src/byui/cit260/darkdungeon/control/MapControl.java
+++ DarkDungeon/src/byui/cit260/darkdungeon/control/MapControl.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * @author Greg K, Bill M, Florian K
  */
 package byui.cit260.darkdungeon.control;
 
+import static java.lang.Math.random;
+
 /**
  *
- * @author gregg
+ * added by Florian
  */
 public class MapControl {
-    
+    public int Move(character,location,monster,health){
+            if (character<1)
+                    {return -1;}
+            if (location<1)
+                    {return -1;}
+            if (monster<1)
+                   {return -1;}
+            if (health<1)
+                    {return -1;}
+            
+            int Move = character + location + monster + health;
+    }
+    public boolean validLocation(character, location){
+        int character = 1;
+            if (character<1)
+                    {return Boolean.FALSE;}
+        int location = 1;
+            if (location<1)
+                    {return Boolean.FALSE;}
+             int validLocation = (int) random();
+             if (validLocation>= 1) {return Boolean.TRUE;}
+                else {return Boolean.TRUE;}
+            
+           
+    }
+     public boolean openLock(){
+         int openLock = (int) Math.random();
+             if (openLock<= 1) {return Boolean.FALSE;}
+                else {return Boolean.TRUE;}
+         
+     }
 }
