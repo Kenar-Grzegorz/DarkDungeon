237d759a99e06ba548cf255314ed78e553c0c2bd
Grzegorz Kenar <gregg@10.0.1.11>
2016-06-08 12:21:43 -0500

    6/08/16 Greg Got a empty class ready for treasurechestopen

---
+++ DarkDungeon/src/byui/cit260/darkdungeon/vew/TreasureChestOpen.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.darkdungeon.vew;
+
+import byui.cit260.darkdungeon.control.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Bill
+ */
+public class TreasureChestOpen {
+
+}
+
