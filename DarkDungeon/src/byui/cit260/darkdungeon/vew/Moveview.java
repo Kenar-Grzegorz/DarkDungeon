31f1f2c7c20fd10bef23ac5b6c54af56461801c7
K <K@192.168.109.106>
2016-06-08 13:45:23 +0100

    Florian 6/8/2016 CIT 260

---
+++ DarkDungeon/src/byui/cit260/darkdungeon/vew/Moveview.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.darkdungeon.vew;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author K
+ */
+public class Moveview {
+    private final String promptMessage;
+    private final String menu;
+    public Moveview() {
+        this.menu = "\n"
+                  + "\n---------------------------------------"
+                  + "\n| Movement                         |"
+                  + "\n---------------------------------------"
+                  + "\nN - North"
+                  + "\nS - South"
+                  + "\nE - East"
+                  + "\nW - West"
+                  + "\n---------------------------------------";
+        
+        this.promptMessage = "Enter Direction you want to go";
+    }
+    
+  public void displayMenu() {
+      boolean done = false; // set flag to not done
+        do{
+            //prompt for and get players name
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q") )// user wants to quit)
+                        return; // exist the game
+            
+            // do the requested action and display the next view
+            done = this. doAction(menuOption);
+        } while (!done);   
+        }
+
+      private String getMenuOption() {
+         Scanner Keyboard = new Scanner (System.in); // get infile for keyboard
+     String value = ""; // value to be returned
+     boolean valid = false; // initialize to not valid
+     
+     while (!valid){ // loop while an invalid value is enter
+         System.out.println("\n" + this.menu);
+         System.out.println("\n" + this.promptMessage);
+         
+         value = Keyboard.nextLine(); // get next line typed on keyboard
+         value = value. trim(); // trim off leading and trailing blanks
+         
+         if (value.length()< 1) { // value is blank
+             System.out.println("\nInvalid value: value can not be blank");
+             continue;    
+         }
+         break; // end the loop
+    }
+    return value;
+      }  
+
+   private boolean doAction(String choice) {
+        choice = choice.toUpperCase(); //convert to uppercase
+        
+        switch (choice) {
+            case "N": //North
+                this.North();
+         break;
+            case "S": //South
+                this.South();
+                break;
+            case "E": //East
+                this.East();
+                break;
+              case "W": //West
+                this.West();
+                break;
+            default:
+                System.out.println("\n*** Invalid Selection *** Try again");
+                break;
+        }
+        return false;
+    }
+
+    private void North() {
+        
+        System.out.println("***You have chosen North***");
+    }
+ private void South() {
+        
+        System.out.println("***You have chosen South***");
+    }
+     private void East() {
+        
+        System.out.println("***You have chosen East***");
+    }
+      private void West() {
+        
+        System.out.println("***You have chosen West***");
+    }
+   
+}
