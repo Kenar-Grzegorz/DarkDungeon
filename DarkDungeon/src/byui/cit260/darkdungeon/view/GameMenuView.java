668d7e6b3f18da5c1b30e9cb18615bd1a42630b8
Grzegorz Kenar <gregg@10.0.1.11>
2016-06-23 19:18:36 -0500

    6/23/16 Greg working on this insane assignment

--- DarkDungeon/src/byui/cit260/darkdungeon/view/GameMenuView.java
+++ DarkDungeon/src/byui/cit260/darkdungeon/view/GameMenuView.java
@@ -10,6 +10,7 @@ import static byui.cit260.darkdungeon.control.GameControl.warrior;
 import byui.cit260.darkdungeon.model.Game;
 import static byui.cit260.darkdungeon.model.Game.*;
 import byui.cit260.darkdungeon.model.Inventory;
+import byui.cit260.darkdungeon.model.Location;
 import darkdungeongame.DarkDungeonGame;
 
 /**
@@ -28,6 +29,7 @@ public class GameMenuView extends View {
                 + "\n|        C. Choose a Character     |"
                 + "\n|        D. Dungeon                |"
                 + "\n|        M. Map                    |"
+                + "\n|        N. Map 2                  |"
                 + "\n|        I. View Inventory         |"
                 + "\n|        Q. Quit                   |"
                 + "\n===================================="
@@ -47,6 +49,9 @@ public class GameMenuView extends View {
                 break;
             case "M":
                 this.map();
+                break;
+            case "N":
+                this.map2();
                 break;
             case "I":
                 this.viewInventory();
@@ -100,14 +105,49 @@ public class GameMenuView extends View {
         
         
         
-    }
-     
-     
+    
+    }  
     private void map() {
         MapView mapView = new MapView();
         mapView.display();
     }
     
+    private void map2() {
+        
+        //get locations from the current game
+        Location[][] locations = DarkDungeonGame.getCurrentGame().getMap().getLocations();
+        
+        //Display title
+        System.out.println(
+                 "\n--------------------"
+                +"\n MAP OF MIDDLE EARTH"
+                +"\n--------------------");
+        //Display row of column numbers
+        System.out.println(" -0  1  2-");
+        //Display row divider
+        System.out.println(" -------");
+        
+        //Display row numbers
+        for (int i = 0; i < locations.length; i++) {
+            //Display column divider
+            System.out.print(i + 1 + "|");
+            
+            for (Location location : locations[i]) {
+                
+                //If location has been visited, display map symbol
+                if (location.getVisited()) {
+                    System.out.print(location.getScene().getMapSymbol());
+                } else {
+                    System.out.print("??");
+                }
+                //Display ending column divider 
+                System.out.print("|");
+            }
+            //Display ending row divider
+            System.out.println("\n -------");
+        }
+    
+    }
     
     
 }
