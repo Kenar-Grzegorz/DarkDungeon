7f25b6abc39e894cba4de48d803267843d23ae1f
Grzegorz Kenar <gregg@10.0.1.11>
2016-06-30 18:46:33 -0500

    6/30/16 Greg touched up Menus

--- DarkDungeon/src/byui/cit260/darkdungeon/control/MapControl.java
+++ DarkDungeon/src/byui/cit260/darkdungeon/control/MapControl.java
@@ -327,41 +327,41 @@ public class MapControl {
 
     }
 
-    public int Move(int character, int location, int monster, int health) {
-        if (character < 1) {
-            return -1;
-        }
-        if (location < 1) {
-            return -1;
-        }
-        if (monster < 1) {
-            return -1;
-        }
-        if (health < 1) {
-            return -1;
-        }
-
-        int Move = character + location + monster + health;
-        return Move;
-    }
-    
-    public boolean validLocation(int character, int location) {
-
-        if (character < 1) {
-            return Boolean.FALSE;
-        }
-
-        if (location < 1) {
-            return Boolean.FALSE;
-        }
-        int validLocation = (int) Math.random();
-        if (validLocation >= 1) {
-            return Boolean.TRUE;
-        } else {
-            return Boolean.FALSE;
-        }
-
-    }
+//    public int Move(int character, int location, int monster, int health) {
+//        if (character < 1) {
+//            return -1;
+//        }
+//        if (location < 1) {
+//            return -1;
+//        }
+//        if (monster < 1) {
+//            return -1;
+//        }
+//        if (health < 1) {
+//            return -1;
+//        }
+//
+//        int Move = character + location + monster + health;
+//        return Move;
+//    }
+//    
+//    public boolean validLocation(int character, int location) {
+//
+//        if (character < 1) {
+//            return Boolean.FALSE;
+//        }
+//
+//        if (location < 1) {
+//            return Boolean.FALSE;
+//        }
+//        int validLocation = (int) Math.random();
+//        if (validLocation >= 1) {
+//            return Boolean.TRUE;
+//        } else {
+//            return Boolean.FALSE;
+//        }
+//
+//    }
 
     public boolean openLock() {
         int openLock = (int) Math.random();
